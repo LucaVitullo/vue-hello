ed2601e9479282e1b6c42e1c4085c011e1f23794
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-24 13:57:20 +0100

    init

---
+++ script.js
@@ -0,0 +1,4 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. :bongo_blob:
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. :cornice_con_disegno:
+*/
